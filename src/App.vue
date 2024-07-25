75c7898f389d3a9f9af1c22ac2c8abbc9ac26d54
mehrnaz98 <mehrnaz.eft98@gmail.com>
2024-07-25 16:15:58 +0330

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,9 @@
 <script setup>
 import { ref } from "vue";
 
-const msg = ref("Shopping List App");
+const header = ref("Shopping List App");
 </script>
 
 <template>
-  <h1>{{ msg }}</h1>
-  <input v-model="msg" />
+  <h1>{{ header }}</h1>
 </template>
