bd9290cf9c87ba88eb4843f95eca242edb20ce8d
mehrnaz98 <mehrnaz.eft98@gmail.com>
2024-07-25 16:25:03 +0330

    Added initial setup for displaying items in the shopping list.

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,14 @@
 import { ref } from "vue";
 
 const header = ref("Shopping List App");
+const items = ref(["milk", "egg", "apple"]);
 </script>
 
 <template>
   <h1>{{ header }}</h1>
+  <ul>
+    <li>
+      {{ items }}
+    </li>
+  </ul>
 </template>
