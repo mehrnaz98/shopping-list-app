e4e19d6456c6219b46d151f0e859cb74065c145c
mehrnaz98 <mehrnaz.eft98@gmail.com>
2024-07-25 16:43:16 +0330

    Refactor items from array of strings to array of objects with unique IDs
    Refactor items from array of strings to array of objects with unique IDs
    
    Updated the items array in App.vue to use objects with unique IDs instead of simple strings. This change improves the data structure by allowing for more complex item attributes and better key management in the v-for loop.

--- src/App.vue
+++ src/App.vue
@@ -2,14 +2,18 @@
 import { ref } from "vue";
 
 const header = ref("Shopping List App");
-const items = ref(["milk", "egg", "apple"]);
+const items = ref([
+  { id: 1, label: "Milk" },
+  { id: 2, label: "Egg" },
+  { id: 3, label: "Apple" },
+]);
 </script>
 
 <template>
   <h1>{{ header }}</h1>
   <ul>
-    <li v-for="(item, index) in items" :key="index">
-      {{ item }}
+    <li v-for="item in items" :key="item.id">
+      {{ item.id }}. {{ item.label }}
     </li>
   </ul>
 </template>
