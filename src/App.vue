abd1376a44e224d133ed04b6d1341bb93d52e70f
mehrnaz98 <mehrnaz.eft98@gmail.com>
2024-07-25 23:53:33 +0330

    Add highPriority property to items and update list styling
    Added `highPriority` property to item objects to indicate priority status. Modified list item styling to apply the `priority` class based on the `highPriority` property, allowing visual differentiation of high-priority items.

--- src/App.vue
+++ src/App.vue
@@ -4,9 +4,9 @@ import { ref } from "vue";
 const header = ref("Shopping List App");
 const editing = ref(false);
 const items = ref([
-  { id: 1, label: "Milk", purchased: true },
-  { id: 2, label: "Egg", purchased: true },
-  { id: 3, label: "Apple", purchased: false },
+  { id: 1, label: "Milk", purchased: true, highPriority: false },
+  { id: 2, label: "Egg", purchased: true, highPriority: false },
+  { id: 3, label: "Apple", purchased: false, highPriority: true },
 ]);
 const newItem = ref("");
 const newItemHighPriority = ref(false);
@@ -50,6 +50,7 @@ const togglePurchased = (item) => {
       @click="togglePurchased(item)"
       :class="{
         strikeout: item.purchased,
+        priority: item.highPriority,
       }"
     >
       {{ item.id }}. {{ item.label }}
