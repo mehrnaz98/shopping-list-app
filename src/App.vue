a92c892243094b1267033be22270c3bc52cb320c
mehrnaz98 <mehrnaz.eft98@gmail.com>
2024-07-25 17:53:35 +0330

    Update form submission to use v-on:submit for adding items
    Moved the click handler from the button to the form element to handle item addition upon form submission. Updated the form to use v-on:submit instead of v-on:click, ensuring that items are added when the form is submitted, whether by clicking the button or pressing Enter in the input field. This refactor improves form submission handling and adheres to standard practices.

--- src/App.vue
+++ src/App.vue
@@ -13,23 +13,16 @@ const newItemHighPriority = ref(false);
 
 <template>
   <h1>{{ header }}</h1>
-  <form class="add-item-form">
-    <input
-      v-model.trim="newItem"
-      v-on:keyup.enter="items.push({ id: items.length + 1, label: newItem })"
-      type="text"
-      placeholder="Add an item"
-    />
+  <form
+    class="add-item-form"
+    v-on:submit="items.push({ id: items.length + 1, label: newItem })"
+  >
+    <input v-model.trim="newItem" type="text" placeholder="Add an item" />
     <label>
       <input type="checkbox" v-model="newItemHighPriority" />
       High Priority
     </label>
-    <button
-      class="btn btn-primary"
-      v-on:click="items.push({ id: items.length + 1, label: newItem })"
-    >
-      Save Item
-    </button>
+    <button class="btn btn-primary">Save Item</button>
   </form>
   <ul>
     <li v-for="item in items" :key="item.id">
